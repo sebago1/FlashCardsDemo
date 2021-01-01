eae1944919ec738bf95aaba5159c853cffa85ba4
Sebastian <sebastian.goras@gmail.com>
2021-01-01 16:20:09 +0000

    js prev/next button

--- js/main.js
+++ js/main.js
@@ -18,7 +18,23 @@ function changeLanguage()
 //  DRAW PREVIOUS BUTTON
 function drawPrev() 
 {
-    
+    var lengthOfTab = polishWords.length - 1;
+
+    if (numberOfWord <= 0)
+    {
+        numberOfWord = lengthOfTab;
+
+        document.getElementById("en").innerHTML = englishWords[numberOfWord];
+        document.getElementById("pl").innerHTML = polishWords[numberOfWord];
+        document.getElementById("pron").innerHTML = pronunciation[numberOfWord];
+    }else
+    {
+        numberOfWord--;
+
+        document.getElementById("en").innerHTML = englishWords[numberOfWord];
+        document.getElementById("pl").innerHTML = polishWords[numberOfWord];
+        document.getElementById("pron").innerHTML = pronunciation[numberOfWord];
+    }
 }
 
 //  DRAW NEXT BUTTON
@@ -26,7 +42,7 @@ function drawNext()
 {
     var lengthOfTab = polishWords.length - 1;
 
-    if(numberOfWord >= lengthOfTab)
+    if (numberOfWord >= lengthOfTab)
     {
         numberOfWord = 0;
 
@@ -57,22 +73,22 @@ function hideOrShowSystem()
 
 //  SMART DATA BASE OF WORDS
 var englishWords = new Array(5);
-englishWords[0] = "Savour";
-englishWords[1] = "Doll";
-englishWords[2] = "Ceating";
-englishWords[3] = "Regret";
-englishWords[4] = "Suicide";
+englishWords[0] = "1 Savour";
+englishWords[1] = "2 Doll";
+englishWords[2] = "3 Ceating";
+englishWords[3] = "4 Regret";
+englishWords[4] = "5 Suicide";
 
 var polishWords = new Array(5);
-polishWords[0] = "Smak";
-polishWords[1] = "Lalka";
-polishWords[2] = "Oszukiwanie";
-polishWords[3] = "Żal";
-polishWords[4] = "Samobójstwo";
+polishWords[0] = "1 Smak";
+polishWords[1] = "2 Lalka";
+polishWords[2] = "3 Oszukiwanie";
+polishWords[3] = "4 Żal";
+polishWords[4] = "5 Samobójstwo";
 
 var pronunciation = new Array(5);
-pronunciation[0] = "Sejwe>r";
-pronunciation[1] = "Dol";
-pronunciation[2] = "Cziting";
-pronunciation[3] = ">Reg>ret";
-pronunciation[4] = "Suisaid";
+pronunciation[0] = "1 Sejwe>r";
+pronunciation[1] = "2 Dol";
+pronunciation[2] = "3 Cziting";
+pronunciation[3] = "4 >Reg>ret";
+pronunciation[4] = "5 Suisaid";
